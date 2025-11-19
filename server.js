f193c0d83f5a5869f0e1c5c62f2cf57f0eb73fa6
jluxury929-hash <jluxury929@gmail.com>
2025-11-18 20:38:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,13 @@
 
 const express = require('express');
 const cors = require('cors');
+const app = express();
+
+app.use(cors()); // ← Add this line
+app.use(express.json());
+
+//const express = require('express');
+const cors = require('cors');
 const { ethers } = require('ethers');
 
 const app = express();
