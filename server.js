cce9468275b9e094c892addb8680f000e1334d81
jluxury929-hash <jluxury929@gmail.com>
2025-11-18 22:38:03 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ const cors = require('cors');
 const { ethers } = require('ethers');
 
 const app = express();
-app.use(cors());
-app.use(express.json());
-
 const PORT = process.env.PORT || 3001;
 
 // PRODUCTION CONFIGURATION
